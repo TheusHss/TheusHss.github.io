1566a75da546b148c0bc228d7d9a4957a09a9b0d
TheusHss <matheus.soares@bandtec.com>
2021-02-21 20:07:58 -0300

    add site de venda

--- projeto-site/public/poesias/script.js
+++ projeto-site/public/poesias/script.js
@@ -1,13 +1,21 @@
 var contTime = 100;
+var contGuarda = 0;
+
 function tempo() {
     setTimeout(() => {
         contTime--
-        if (contTime <= 0) {
+        if (contTime <= 0 && contGuarda == 1) {
             document.querySelectorAll(".box").forEach(iten => iten.style.display = "none")
             if (cont % 2 == 0) {
-                vez.innerHTML = `PARABÉNS AO JOGADOR ${registro[0].player_two}`
-            } else {
-                vez.innerHTML = `PARABÉNS AO JOGADOR ${registro[0].player_one}`
+                b1++
+
+                b_j.innerHTML = `J1=${b1}`
+                vez.innerHTML = `PARABÉNS AO JOGADOR <br> ${registro[0].player_two}`
+            } else if(cont % 2==1) {
+                b2++
+
+                b_jj.innerHTML = `J2=${b2}`
+                vez.innerHTML = `PARABÉNS AO JOGADOR <br> ${registro[0].player_one}`
 
             }
 
@@ -29,6 +37,7 @@ function tempo() {
 var registro = []
 function resetar() {
     cont = 0
+    contGuarda = 0
     contTime = 100
     um.innerHTML = ""
     dois.innerHTML = ""
@@ -61,9 +70,9 @@ function iniciar() {
 var cont = 0;
 function box_click() {
     if (cont % 2 == 0) {
-        vez.innerHTML = `VEZ DO JOGADOR ${registro[0].player_one}`
+        vez.innerHTML = `VEZ DO JOGADOR <br>${registro[0].player_one}`
     } else {
-        vez.innerHTML = `VEZ DO JOGADOR ${registro[0].player_two}`
+        vez.innerHTML = `VEZ DO JOGADOR <br>${registro[0].player_two}`
 
     }
 }
@@ -228,15 +237,29 @@ function um_click() {
     }
     verificarVitoria()
 }
-function verificarVitoria(){
-    if(nove.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  oito.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  sete.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || seis.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  quatro.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  dois.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  um.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  quatro.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  sete.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || dois.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  oito.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  seis.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  nove.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&& cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  nove.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`  || tres.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`&&  cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` &&  sete.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`){
-      contTime=0
-      ganhou.play()
-    }else if(nove.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  oito.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  sete.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || seis.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  quatro.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  dois.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  um.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  quatro.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  sete.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || dois.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  oito.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  seis.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  nove.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&& cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  nove.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`  || tres.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`&&  cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` &&  sete.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`){
-        contTime=0
-      ganhou.play()
+var b1 = 0;
+var b2 = 0;
+var b3 = 0;
+function verificarVitoria() {
+    if (nove.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && oito.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && sete.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || seis.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && quatro.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && dois.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && um.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && quatro.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && sete.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || dois.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && oito.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && seis.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && nove.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && nove.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/x.png" alt="">` && sete.innerHTML == `<img class="img_click" src="./img/x.png" alt="">`) {
+        contTime = 0
+        contGuarda++
+        ganhou.play()
+    } else if (nove.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && oito.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && sete.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || seis.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && quatro.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && dois.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && um.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && quatro.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && sete.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || dois.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && oito.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && seis.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && nove.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || um.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && nove.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` || tres.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && cinco.innerHTML == `<img class="img_click" src="./img/o.png" alt="">` && sete.innerHTML == `<img class="img_click" src="./img/o.png" alt="">`) {
+        contTime = 0
+        contGuarda++
 
-      }
+        // b2++
+        // b_jj.innerHTML=`J2=${b2}`
+        ganhou.play()
+
+    } else if (cont == 9) {
+        contGuarda++
+
+        b3++
+        b_v.innerHTML = `V=${b3}`
+        resetar()
+    }
 }
 
 
