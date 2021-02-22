7dc61777a4369906eb0ccff4555c67a3c3a917c7
TheusHss <matheus.soares@bandtec.com>
2021-02-22 18:15:22 -0300

    modificação de tela

---
+++ projeto-site/public/tabacaria/script.js
@@ -0,0 +1,41 @@
+function narguiles_func() {
+
+    window.location.href = "./index.html#narguiles"
+
+}
+function carvao_func() {
+
+    window.location.href = "./index.html#carvao"
+
+}
+function essencias_func() {
+
+    window.location.href = "./index.html#essencias"
+
+}
+function contato_func() {
+
+    window.location.href = "./index.html#contato"
+
+}
+function exclusivo() {
+
+
+}
+function chamar_menu() {
+    document.querySelectorAll.foreach('#boxMenuId').classList.toggle("boxMenuAtivo")
+}
+function face() {
+    window.location.href = "https://www.facebook.com/allahubhookah6/"
+} function insta() {
+    window.location.href = "https://www.instagram.com/allahubhookah6/"
+}
+function boxClickOne(){
+    document.querySelector('#oneBox').classList.toggle("boxGrande")
+}
+function boxClickTwo(){
+    document.querySelector('#twoBox').classList.toggle("boxGrande")
+}
+function boxClickThree(){
+    document.querySelector('#threeBox').classList.toggle("boxGrande")
+}
